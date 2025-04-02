7c83f570264bc1d3897078e0c742b5faa4c99c30
angelraiyanii <araiyani764@rku.ac.in>
2025-04-02 10:58:13 +0530

    order History

---
+++ lib/screens/orderhistory.dart
@@ -0,0 +1,410 @@
+import 'package:cosmic_pages/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class orderhistory extends StatefulWidget {
+  const orderhistory({super.key});
+
+  @override
+  State<orderhistory> createState() => _orderhistoryState();
+}
+
+class _orderhistoryState extends State<orderhistory> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Get.to(home());
+          },
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              print(value);
+            },
+            itemBuilder: (BuildContext context) {
+              return ['Edit Profile', 'Setting', 'Logout']
+                  .map((String choice) => PopupMenuItem<String>(
+                        value: choice,
+                        child: Text(choice),
+                      ))
+                  .toList();
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Order History",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      "assets/images/p1.png",
+                      width: 140,
+                      height: 120,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Author Name",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "angel@gmail.com",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          "\$120",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Purchased on 31 Feb",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                            Text(
+                              "2023 2:00 AM",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      PopupMenuButton<String>(
+                        onSelected: (value) {
+                          print(value);
+                        },
+                        itemBuilder: (BuildContext context) {
+                          return ['Edit Profile', 'Setting', 'Logout']
+                              .map((String choice) => PopupMenuItem<String>(
+                                    value: choice,
+                                    child: Text(choice),
+                                  ))
+                              .toList();
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      "assets/images/p1.png",
+                      width: 140,
+                      height: 120,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Author Name",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "angel@gmail.com",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          "\$120",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Purchased on 31 Feb",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                            Text(
+                              "2023 2:00 AM",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      PopupMenuButton<String>(
+                        onSelected: (value) {
+                          print(value);
+                        },
+                        itemBuilder: (BuildContext context) {
+                          return ['Edit Profile', 'Setting', 'Logout']
+                              .map((String choice) => PopupMenuItem<String>(
+                                    value: choice,
+                                    child: Text(choice),
+                                  ))
+                              .toList();
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      "assets/images/p3.png",
+                      width: 140,
+                      height: 120,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Author Name",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "angel@gmail.com",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          "\$120",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Purchased on 31 Feb",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                            Text(
+                              "2023 2:00 AM",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      PopupMenuButton<String>(
+                        onSelected: (value) {
+                          print(value);
+                        },
+                        itemBuilder: (BuildContext context) {
+                          return ['Edit Profile', 'Setting', 'Logout']
+                              .map((String choice) => PopupMenuItem<String>(
+                                    value: choice,
+                                    child: Text(choice),
+                                  ))
+                              .toList();
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      "assets/images/p1.png",
+                      width: 140,
+                      height: 120,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Author Name",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "angel@gmail.com",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          "\$120",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Purchased on 31 Feb",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                            Text(
+                              "2023 2:00 AM",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      PopupMenuButton<String>(
+                        onSelected: (value) {
+                          print(value);
+                        },
+                        itemBuilder: (BuildContext context) {
+                          return ['Edit Profile', 'Setting', 'Logout']
+                              .map((String choice) => PopupMenuItem<String>(
+                                    value: choice,
+                                    child: Text(choice),
+                                  ))
+                              .toList();
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      "assets/images/p3.png",
+                      width: 140,
+                      height: 140,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Author Name",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "angel@gmail.com",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          "\$120",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Purchased on 31 Feb",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                            Text(
+                              "2023 2:00 AM",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      PopupMenuButton<String>(
+                        onSelected: (value) {
+                          print(value);
+                        },
+                        itemBuilder: (BuildContext context) {
+                          return ['Edit Profile', 'Setting', 'Logout']
+                              .map((String choice) => PopupMenuItem<String>(
+                                    value: choice,
+                                    child: Text(choice),
+                                  ))
+                              .toList();
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
